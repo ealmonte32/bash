d047a1865b67f0509774d9c14b358ea81952dd80
Emyll Almonte <eg0dz@aol.com>
2020-02-13 16:20:26 -0500

    Update find500wingipcam.sh

--- find500wingipcam.sh
+++ find500wingipcam.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+#ealmonte32
+
 rm -rf ./500wingcamerasfound.txt; rm -rf ./500wingnoresponse.txt;
+
 for i in {10..249}
 do
 ping -t 1 -c 1 192.168.50.$i >/dev/null;
 if [ $? != 0 ]
-then echo 192.168.50.$i >> 500wingnoresponse.txt;
-else curl -s --digest --user admin:admin http://192.168.50.$i | grep -iq sensor && echo 192.168.50.$i >> 500wingcamerasfound.txt;
+then
+echo 192.168.50.$i >> 500wingnoresponse.txt;
+else
+curl -s --digest --user admin:admin http://192.168.50.$i | grep -iq sensor && echo 192.168.50.$i >> 500wingcamerasfound.txt;
 fi
 done
-
