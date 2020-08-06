183fcfe90c955ea10f184e0ec0c80fb7d31c7919
Emyll Almonte <eg0dz@aol.com>
2020-08-06 14:00:23 -0400

    Add files via upload
    example manipulating array from a file with adding and removing elements and checking if element already exists in array and only removing exact word entered.

--- case_selection_sample.sh
+++ case_selection_sample.sh
@@ -23,14 +23,26 @@ add_element()
     echo -n "Enter a word to add to the array and press enter: "
     read entereditem
     echo "Adding entered item to array..."
-    echo "$entereditem" >> ~/array_list.txt
-    echo "Done."
-    echo -n "Would you like to enter another item? (Press y for yes or any other key for no)"
-    read -n 1 enteranother
-    case $enteranother in
-	y ) add_element ;;
-	* ) exit ;;
-    esac
+    CHECK=$(printf '%s\n' "${ARRAY[@]}" | grep "$entereditem$") #adding the dollar sign at end makes sure that it only finds the exact match
+    if [[ -z "$CHECK" ]] #if the entered word was not found in the list - continue to add it
+    then
+	echo "$entereditem" >> ~/array_list.txt
+    	echo "Done."
+    	echo -en "Would you like to enter another item? (Press y for yes or any other key for no)\n"
+    	read -n 1 enteranother
+    	case $enteranother in
+		y ) clear; add_element ;;
+		* ) clear ;;
+    	esac
+    else #if word was found in array already - throw error
+	echo "Error, that word is already on the array list."
+	echo -en "Would you like to try again? (Press y for yes or any other key for no)\n"
+	read -n 1 tryagain
+        case $tryagain in
+                y ) clear; add_element ;;
+                * ) clear ;;
+        esac
+    fi
 }
 
 remove_element()
@@ -41,7 +53,7 @@ remove_element()
     echo -n "Enter the exact name of the item in the array you would like to remove: "
     read entereditem
     echo "Removing entered item from array..."
-    CHECK=$(printf '%s\n' "${ARRAY[@]}" | grep "$entereditem")
+    CHECK=$(printf '%s\n' "${ARRAY[@]}" | grep "$entereditem$") #adding the dollar sign at end makes sure that it only finds the exact match
     if [[ -z "$CHECK" ]] #if the entered word was not found in the list
     then
 	echo "That item was not found on the array, try again.";
@@ -73,7 +85,7 @@ read selection
 echo ""
 case $selection in
    1 ) clear; read_array_list ; echo -e "Array contains: \n"; printf '%s\n' "${ARRAY[@]}" ; press_enter ;;
-   2 ) clear; add_element ; press_enter ;;
+   2 ) clear; add_element ;;
    3 ) clear; read_array_list ; remove_element ; press_enter ;;
    0 ) exit ;;
    * ) echo "Incorrect selection, selection must be 1,2,3 or 0."; press_enter
