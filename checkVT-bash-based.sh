6211df0e249870e5ea570bacb650b686f8e64334
Emyll Almonte <eg0dz@aol.com>
2020-08-20 01:55:30 -0400

    Add files via upload

--- checkVT-bash-based.sh
+++ checkVT-bash-based.sh
@@ -38,15 +38,15 @@ _VTBASEURL=https://www.virustotal.com/gui/url/
 
 # get input of URL to check
 read -p "Enter the URL to check: " _REGURL
-echo -n "The entered URL was: ${_REGURL}" >> ~/Desktop/checkvt.log
+echo "The entered URL was: ${_REGURL}" >> ~/Desktop/checkvt.log
 
 # check if the URL doesnt have http or https protocol
 if [[ "${_REGURL}" != *'http:'* && "${_REGURL}" != *'https:'* ]]
 then
 	# add http at beginning of REGURL because almost all https enforced sites will redirect from their http
 	_REGURL="http://${_REGURL}"
-	echo -n "The entered URL did not contain http or https." >> ~/Desktop/checkvt.log
-	echo -n "http has been added, url is now: ${_REGURL}" >> ~/Desktop/checkvt.log
+	echo "The entered URL did not contain http or https." >> ~/Desktop/checkvt.log
+	echo "http has been added, url is now: ${_REGURL}" >> ~/Desktop/checkvt.log
 fi
 
 
@@ -57,15 +57,15 @@ _CHECKURL=$(curl -m 5 -skI "$_REGURL" | awk '/^[Ll]ocation:/ {sub("\r", "", $2);
 # check if checkurl is empty, if so, it means no redirect location was found
 if [[ -z "${_CHECKURL}" ]]
 then
-	echo -n "(no redirect): the checked url does not contain any redirect." >> ~/Desktop/checkvt.log
+	echo "(no redirect): the checked url does not contain any redirect." >> ~/Desktop/checkvt.log
 
 	# the regular entered url needs to end with a /
 	if [[ "${_REGURL}" != *'/' ]] # this wildcard checks the end of a string for character /
 	then
 		# add / at end of the entered URL if it is not found
 		_REGURL+='/'
-		echo -n "The entered URL did not contain a / at end, so it was added." >> ~/Desktop/checkvt.log
-		echo -n "The url is now ${_REGURL}" >> ~/Desktop/checkvt.log
+		echo "The entered URL did not contain a / at end, so it was added." >> ~/Desktop/checkvt.log
+		echo "The url is now ${_REGURL}" >> ~/Desktop/checkvt.log
 	fi
 
 	# since no redirect exist, we use _REGURL
@@ -76,12 +76,12 @@ then
 	_FINALURL="${_VTBASEURL}${_SHAURL}"
 
 	# then we open the combined final URL
-	echo -n "The non-redirected final URL was: ${_FINALURL}" >> ~/Desktop/checkvt.log
+	echo "The non-redirected final URL was: ${_FINALURL}" >> ~/Desktop/checkvt.log
 	open "${_FINALURL}"
 
 else
-	echo -n "(redirect found): the checked url variable contains a redirect to: ${_CHECKURL}" >> ~/Desktop/checkvt.log
-	echo -n "The new redirected URL is now ${_CHECKURL}" >> ~/Desktop/checkvt.log
+	echo "(redirect found): the checked url variable contains a redirect to: ${_CHECKURL}" >> ~/Desktop/checkvt.log
+	echo "The new redirected URL is now ${_CHECKURL}" >> ~/Desktop/checkvt.log
 
 	# while the URL contains any reference to a redirect location, keep on assigning it to the variable
 	while [[ ! -z "${_CHECKURL}" ]]
@@ -91,10 +91,10 @@ else
 		then
 			_LASTCHECKURL="${_CHECKURL}"
 			unset _CHECKURL # we clear the variable so that the while loop stops
-			echo -n "no more redirects found" >> ~/Desktop/checkvt.log
+			echo "no more redirects found" >> ~/Desktop/checkvt.log
 		else
 			_CHECKURL="${_REDIRECTURL}"
-			echo -n "(redirect found): the URL contains a redirect to ${_REDIRECTURL}" >> ~/Desktop/checkvt.log
+			echo "(redirect found): the URL contains a redirect to ${_REDIRECTURL}" >> ~/Desktop/checkvt.log
 		fi
 
 	done
@@ -104,16 +104,16 @@ else
 	then
 		# add http at beginning of REGURL because almost all https enforced sites will redirect from their http
 		_LASTCHECKURL="http://${_LASTCHECKURL}"
-		echo -n "The last checked URL did not contain http or https." >> ~/Desktop/checkvt.log
-		echo -n "http has been added, URL is now: ${_LASTCHECKURL}" >> ~/Desktop/checkvt.log
+		echo "The last checked URL did not contain http or https." >> ~/Desktop/checkvt.log
+		echo "http has been added, URL is now: ${_LASTCHECKURL}" >> ~/Desktop/checkvt.log
 	fi
 
 	# check the last checked URL for / at end
 	if [[ "${_LASTCHECKURL}" != *'/' ]] # if last checked URL does not contain / at end, we need to add it
 	then
 		_LASTCHECKURL+='/'
-		echo -n "The redirect URL did not contain a / at end." >> ~/Desktop/checkvt.log
-		echo -n "Fixed, redirect URL is now: ${_LASTCHECKURL}" >> ~/Desktop/checkvt.log
+		echo "The redirect URL did not contain a / at end." >> ~/Desktop/checkvt.log
+		echo "Fixed, redirect URL is now: ${_LASTCHECKURL}" >> ~/Desktop/checkvt.log
 	fi
 
 	# redirect existed, so we use the last checked URL which is inside the _LASTCHECKURL variable already
@@ -123,7 +123,7 @@ else
 	_FINALURL="${_VTBASEURL}${_SHAURL}"
 
 	# then we open the combined final URL
-	echo -n "The redirected final URL was: ${_FINALURL}" >> ~/Desktop/checkvt.log
+	echo "The redirected final URL was: ${_FINALURL}" >> ~/Desktop/checkvt.log
 	open "${_FINALURL}"
 fi
 
